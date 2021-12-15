45fdbe41579e1ad9bf882c09e3ddc29364911e58
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2021-12-15 17:41:45 +0100

    Update commentaire.php

--- commentaire.php
+++ commentaire.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-if (!isset($_SESSION['id'])) {
+if (!isset($_SESSION['id']))
+{
   header('Location:index.php');
   die(); // Inaccesibilité en cas de session absente ou fausse. Redirection vers index. ❌
 }
@@ -8,13 +9,15 @@ $bdd = new PDO('mysql:host=localhost;dbname=livreor;charset=utf8', 'root', ''); 
 $request = $bdd->prepare('SELECT * FROM utilisateurs WHERE id = ?');
 $request->execute(array($_SESSION['id']));
 $data = $request->fetch();
-if (!empty($_POST['comm'])) {
+if (!empty($_POST['comm']))
+{
   // Commentaire
   $comm = htmlspecialchars($_POST['write-comm']); // "htmlspecialchars" la sécurité habituelle pour empêcher d'éxecuter du code sur les champs.
   // Date
-  $dt = new DateTime();
+  $quand = new DateTime();
   // Date et heure 🕑📆
-  $dt->format('Y-m-d H:i:s'); {
+  $quand->format('Y-m-d H:i:s');
+  {
     if (strlen($comm) >= 1) // Minimum 1 caractère.
       $insert = $bdd->prepare('INSERT INTO commentaires(commentaire,id_utilisateur,date) VALUES(:commentaire,:id_utilisateur,NOW())');
     $insert->execute(array('commentaire' => $comm, 'id_utilisateur' => intval($data['id'])));
