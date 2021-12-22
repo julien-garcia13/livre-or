5ffe55001b3af72e7e6fbad9550c472e6005f8ec
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2021-12-22 13:58:03 +0100

    Add files via upload

--- livre-or.php
+++ livre-or.php
@@ -9,62 +9,67 @@ $data = $request->fetch();
 ?>
 <!-- Début du HTML. -->
 <html lang="fr">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Commentaires - Livre d'Or</title>
-  <style>
-    @keyframes move-background {
-      from {
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Commentaires - Livre d'Or</title>
+    <!-- CSS -->
+    <style>
+    @keyframes move-background
+    {
+      from
+      {
         -webkit-transform: translate3d(0px, 0px, 0px);
       }
-
-      to {
+      to
+      {
         -webkit-transform: translate3d(1000px, 0px, 0px);
       }
     }
-
-    @-webkit-keyframes move-background {
-      from {
+    @-webkit-keyframes move-background
+    {
+      from
+      {
         -webkit-transform: translate3d(0px, 0px, 0px);
       }
-
-      to {
+      to
+      {
         -webkit-transform: translate3d(1000px, 0px, 0px);
       }
     }
-
-    @-moz-keyframes move-background {
-      from {
+    @-moz-keyframes move-background
+    {
+      from
+      {
         -webkit-transform: translate3d(0px, 0px, 0px);
       }
-
-      to {
+      to
+      {
         -webkit-transform: translate3d(1000px, 0px, 0px);
       }
     }
-
-    @-webkit-keyframes move-background {
-      from {
+    @-webkit-keyframes move-background
+    {
+      from
+      {
         -webkit-transform: translate3d(0px, 0px, 0px);
       }
-
-      to {
+      to
+      {
         -webkit-transform: translate3d(1000px, 0px, 0px);
       }
     }
-
-    .background-container {
+    .background-container
+    {
       position: fixed;
       top: 0;
       left: 0;
       bottom: 0;
       right: 0;
     }
-
-    .stars {
+    .stars
+    {
       background: black url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/stars.png) repeat;
       position: absolute;
       top: 0;
@@ -74,8 +79,8 @@ $data = $request->fetch();
       display: block;
       z-index: 0;
     }
-
-    .twinkling {
+    .twinkling
+    {
       width: 10000px;
       height: 100%;
       background: transparent url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/twinkling.png") repeat;
@@ -91,8 +96,8 @@ $data = $request->fetch();
       -webkit-animation: move-background 70s linear infinite;
       animation: move-background 70s linear infinite;
     }
-
-    .clouds {
+    .clouds
+    {
       width: 10000px;
       height: 100%;
       background: transparent url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/clouds_repeat.png") repeat;
@@ -108,16 +113,16 @@ $data = $request->fetch();
       -webkit-animation: move-background 150s linear infinite;
       animation: move-background 150s linear infinite;
     }
-
-    .moon {
+    .moon
+    {
       height: 70vh;
       width: 70vh;
       position: relative;
       z-index: 3;
       right: 20px;
     }
-
-    p1 {
+    p1
+    {
       display: flex;
       flex-direction: column;
       flex-wrap: wrap;
@@ -130,28 +135,30 @@ $data = $request->fetch();
       -moz-animation: glow 1s ease-in-out infinite alternate;
       animation: glow 1s ease-in-out infinite alternate;
     }
-
-    @-webkit-keyframes glow {
-      from {
+    @-webkit-keyframes glow
+    {
+      from
+      {
         text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;
       }
 
-      to {
+      to
+      {
         text-shadow: 0 0 20px #fff, 0 0 30px #ff4da6, 0 0 40px #ff4da6, 0 0 50px #ff4da6, 0 0 60px #ff4da6, 0 0 70px #ff4da6, 0 0 80px #ff4da6;
       }
     }
-
-    @font-face {
+    @font-face
+    {
       font-family: "gwendolyn";
       src: url("font/gwendolyn-bold-webfont.woff") format("woff"), url("font/gwendolyn-bold-webfont.woff2") format("woff2");
     }
-
-    @font-face {
+    @font-face
+    {
       font-family: "notosans";
       src: url("font/notosans-regular-webfont.woff") format("woff"), url("font/notosans-regular-webfont.woff2") format("woff2");
     }
-
-    footer {
+    footer
+    {
       width: 100%;
       height: 64px;
       display: flex;
@@ -159,9 +166,9 @@ $data = $request->fetch();
       margin-top: 14%;
       position: absolute;
     }
-
-    .cadre {
-      background-color: rgba(255, 255, 255, 0.3);
+    .cadre
+    {
+      background-color: rgba(255, 255, 255, 0.8);
       display: flex;
       justify-content: center;
       margin-top: 3%;
@@ -172,41 +179,41 @@ $data = $request->fetch();
       color: white;
       position: relative;
     }
-
-    .comms {
+    .comms
+    {
       display: flex;
       justify-content: center;
+      flex-direction: row;
       align-items: center;
-      width: 100%;
-      height: 400px;
+      color: 512b36;
     }
-
-    .utilisateur {
+    .utilisateur
+    {
       color: 224860;
       font-family: "notosans";
       font-size: 20px;
     }
-
-    .date {
+    .date
+    {
       color: black;
       font-family: "notosans";
       font-size: 20px;
     }
-
-    .comm-en-question {
+    .comm-en-question
+    {
       font-family: "notosans";
       font-size: 20px;
     }
-
-    .champ-a-remplir {
+    .champ-a-remplir
+    {
       display: flex;
       flex-direction: row;
       margin-top: 15%;
       height: 100px;
       position: absolute;
     }
-
-    .post {
+    .post
+    {
       background: #1AAB8A;
       color: #fff;
       border: none;
@@ -218,14 +225,14 @@ $data = $request->fetch();
       transition: 800ms ease all;
       outline: none;
     }
-
-    .post:hover {
+    .post:hover
+    {
       background: #fff;
       color: #1AAB8A;
     }
-
     .post:before,
-    button:after {
+    button:after
+    {
       content: '';
       position: absolute;
       top: 0;
@@ -235,16 +242,16 @@ $data = $request->fetch();
       background: #1AAB8A;
       transition: 400ms ease all;
     }
-
-    .post:after {
+    .post:after
+    {
       right: inherit;
       top: inherit;
       left: 0;
       bottom: 0;
     }
-
     .post:hover:before,
-    .post:hover:after {
+    .post:hover:after
+    {
       width: 100%;
       transition: 800ms ease all;
     }
@@ -257,46 +264,45 @@ $data = $request->fetch();
       margin-top: 14%;
       position: absolute;
     }
-  </style>
-</head>
-
-<body>
-  <div class="background-container">
-    <img class="moon" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png" alt="">
-    <div class="stars"></div>
-    <div class="twinkling"></div>
-    <div class="clouds"></div>
-  </div>
-  <main>
-    <p1>Commentaires</p1><br />
-    <article>
-      <div class="cadre">
-        <div class="comms">
-          <?php
-          // Je sélectionne la BDD commentaires pour en récupérer les données.
-          $request = $bdd->query('SELECT commentaires.commentaire,utilisateurs.login,commentaires.date FROM commentaires LEFT JOIN utilisateurs ON commentaires.id_utilisateur = utilisateurs.id');
-          ?>
-          <?php
-          while ($data = $request->fetch())
-          {
-            // Liste des commentaires
-            echo "<ul><b><p2 class=\"utilisateur\">Utilisateur : " . $data['login'] . "</p2></b></ul><br />
-            <ul><b><p2 class= \"date\">Le : " . $data['date'] . "</p2></b></ul><br />
-            <ul><b><p2 class= \"comm-en-question\">" . $data['commentaire'] . "</p2></b></ul><br />";
-          }
-          ?>
-        </div>
-        <div class="champ-a-remplir">
-          <form method="POST" action="commentaire.php" style ="align-items:center">
-            <input type="text" name="write-comm" required="required" autocomplete="off" placeholder="Commentaire..."><br /></input>
-            <input class="post" type="submit" name="comm"></input>
-        </form>
-        </div>
+    </style>
+    </head>
+    <body>
+      <div class="background-container">
+        <img class="moon" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png" alt="">
+        <div class="stars"></div>
+        <div class="twinkling"></div>
+        <div class="clouds"></div>
       </div>
-    </article>
-  </main>
-  <footer>
-    <a href="https://github.com/julien-garcia13/livre-or"><img class="GitHub" src="GitHub_Logo.png" alt="logo"></img></a>
-  </footer>
-</body>
-</html>
+      <main>
+        <p1>Commentaires</p1><br />
+        <article>
+          <div class="cadre">
+            <div class="comms">
+              <?php
+              // Je sélectionne la BDD commentaires pour en récupérer les données.
+              $request = $bdd->query('SELECT commentaires.commentaire,utilisateurs.login,commentaires.date FROM commentaires LEFT JOIN utilisateurs ON commentaires.id_utilisateur = utilisateurs.id');
+              ?>
+              <?php
+              while ($data = $request->fetch())
+              {
+                // Liste des commentaires
+                echo "<div><ul><b><p2 class=\"utilisateur\">Utilisateur : " . $data['login'] . "</p2></b></ul></div><br />
+                <div><ul><b><p2 class= \"date\">Le : " . $data['date'] . "</p2></b></ul></div><br />
+                <div><ul><b><p2 class= \"comm-en-question\">" . $data['commentaire'] . "</p2></b></ul></div><br />";
+              }
+              ?>
+              </div>
+              <div class="champ-a-remplir">
+                <form method="POST" action="commentaire.php" style ="align-items:center">
+                <input type="text" name="write-comm" required="required" autocomplete="off" placeholder="Commentaire..."><br /></input>
+                <input class="post" type="submit" name="comm"></input>
+              </form>
+            </div>
+          </div>
+        </article>
+      </main>
+      <footer>
+        <a href="https://github.com/julien-garcia13/livre-or"><img class="GitHub" src="GitHub_Logo.png" alt="logo"></img></a>
+      </footer>
+    </body>
+    </html>
